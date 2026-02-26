e2cecc099f4f46a147442ef319dfeceeb16099f6
okdmfp <k29399591@gmail.com>
2026-02-26 15:46:30 +0000

    消えてしまったコントローラー等を復元

--- Models/Company.php
+++ Models/Company.php
@@ -1,25 +1,6 @@
 <?php
-
 namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-class Company extends Model
-{
-    use HasFactory;
-
-    protected $fillable = [
-        'company_name',
-    ];
-
-    public function users()
-    {
-        return $this->hasMany(User::class);
-    }
-
-    public function products()
-    {
-        return $this->hasMany(Product::class);
-    }
+class Company extends Model {
+    // Companyのコード
 }
